543676a4a444aaf39cb65987b1144aa8d8df077a
martabereta <Marta.Bereta@pl.ibm.com>
2020-07-15 11:06:08 +0200

    Checking K8s cluster if ready

--- scripts/check_cluster.sh
+++ scripts/check_cluster.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
 ibmcloud ks cluster get --cluster $1 | grep ^State
+
+EXPECTED_STATE='normal'
+ACTUAL_STATE='ibmcloud ks cluster get --cluster $1 | grep ^State'
+until [ "$ACTUAL_STATE" != *"$EXPECTED_STATE"* ]
+do
+  echo "I'm waiting for one minute"
+  sleep 1m
+  ACTUAL_STATE = 'ibmcloud ks cluster get --cluster $1 | grep ^State'
+done
+
+echo "Cluster is ready!"
