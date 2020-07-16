676c38d76e67eb72e7b89bbe2d26cc2fb1444297
martabereta <Marta.Bereta@pl.ibm.com>
2020-07-16 10:32:36 +0200

    Waiting for cluster to be ready

--- scripts/check_cluster.sh
+++ scripts/check_cluster.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 EXPECTED_STATE='normal'
-#EXPECTED_STATE='deploying'
 ACTUAL_STATE="$(ibmcloud ks cluster get --cluster $1 | grep ^State)"
 ACTUAL_STATE="${ACTUAL_STATE#*:}"
-echo "Expected state" $EXPECTED_STATE
-echo "Actual state" $ACTUAL_STATE
+echo "Expected state: " $EXPECTED_STATE
+echo "Current state: " $ACTUAL_STATE
 
-
+total_nb_of_minutes=0
+time_to_wait=1m
 until [ $ACTUAL_STATE == $EXPECTED_STATE ]
 do
-  echo "I'm waiting for one minute"
-  sleep 1m
+  echo "Waiting for " $time_to_wait " to check if cluster is in '" $EXPECTED_STATE "' state."
+  sleep $time_to_wait
+  ((total_nb_of_minutes++))
   ACTUAL_STATE="$(ibmcloud ks cluster get --cluster $1 | grep ^State)"
   ACTUAL_STATE="${ACTUAL_STATE#*:}"
-  echo "Updated actual state" $ACTUAL_STATE
-  #EXPECTED_STATE="normal"
-  #sleep 5
+  echo "... current state: " $ACTUAL_STATE
+  EXPECTED_STATE="normal"
 done
 
-echo "Cluster is ready!"
+echo "Cluster turned into '" $EXPECTED_STATE "' state after " $total_nb_of_minutes " minute(s)"
