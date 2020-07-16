eedb7a0c31ff72bf51cdd3fa9a7f2d8be262d35f
martabereta <Marta.Bereta@pl.ibm.com>
2020-07-16 10:02:51 +0200

    Checking K8s cluster if ready

--- scripts/check_cluster.sh
+++ scripts/check_cluster.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-ibmcloud ks cluster get --cluster $1 | grep ^State
-
 EXPECTED_STATE='normal'
-ACTUAL_STATE='ibmcloud ks cluster get --cluster $1 | grep ^State'
-until [ "$ACTUAL_STATE" != *"$EXPECTED_STATE"* ]
+#EXPECTED_STATE='deploying'
+ACTUAL_STATE="$(ibmcloud ks cluster get --cluster $1 | grep ^State)"
+ACTUAL_STATE="${ACTUAL_STATE#*:}"
+echo "Expected state" $EXPECTED_STATE
+echo "Actual state" $ACTUAL_STATE
+
+
+until [ $ACTUAL_STATE == $EXPECTED_STATE ]
 do
   echo "I'm waiting for one minute"
   sleep 1m
-  ACTUAL_STATE = 'ibmcloud ks cluster get --cluster $1 | grep ^State'
+  ACTUAL_STATE="$(ibmcloud ks cluster get --cluster $1 | grep ^State)"
+  ACTUAL_STATE="${ACTUAL_STATE#*:}"
+  echo "Updated actual state" $ACTUAL_STATE
+  #EXPECTED_STATE="normal"
+  #sleep 5
 done
 
 echo "Cluster is ready!"
